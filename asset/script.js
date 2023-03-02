adf649e2a091bb097bb157d713eec242807ede47
Zaelly <zazabarbosa2@gmail.com>
2023-03-02 15:09:10 -0300

    nao alterei muita coisa

--- asset/script.js
+++ asset/script.js
@@ -1,5 +1,5 @@
 //botao que habilita um termo de condições
-        function checkbotao() {
+        document.getElementById("checkbox").onclick = function checkbotao() {
             if (document.getElementById('check').checked == true) {
               document.getElementById('envia').disabled = ""
             }
@@ -10,12 +10,12 @@
           }
 
 // olho
-        document.getElementById("olho").onclick = function() {
+        document.getElementById("olho").onmouseover = function() {
             let obj = document.getElementById('password');
             obj.type = "text";
         };
     
-        document.getElementById("olho").onclick = function (){
+        document.getElementById("olho").onmouseout = function (){
         let obj = document.getElementById('password');
         obj.type = "password";
         };
