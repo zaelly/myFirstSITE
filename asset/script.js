d74e2d834fe51145c9043224f8e9b7b6d8a7d805
Zaelly <zazabarbosa2@gmail.com>
2023-03-03 11:13:24 -0300

    fazendo alterações no js

--- asset/script.js
+++ asset/script.js
@@ -1,5 +1,5 @@
 //botao que habilita um termo de condições
-        document.getElementById("checkbox").onclick = function checkbotao() {
+          function checkbotao() {
             if (document.getElementById('check').checked == true) {
               document.getElementById('envia').disabled = ""
             }
@@ -11,11 +11,24 @@
 
 // olho
         document.getElementById("olho").onmouseover = function() {
-            let obj = document.getElementById('password');
+            let obj = document.getElementById('password').innerHTML;
             obj.type = "text";
         };
     
         document.getElementById("olho").onmouseout = function (){
-        let obj = document.getElementById('password');
+        let obj = document.getElementById('password').innerHTML;
         obj.type = "password";
         };
+
+// outra forma
+
+        //  function mouseoutPass () {
+        //     let obj = document.getElementById('password');
+        //     obj.type = "text";
+        // };
+
+        //  function mouseoverPass () {
+        //   let obj = document.getElementById('password');
+        //   obj.type = "password";
+        //   };
+  
